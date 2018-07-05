84d50e230288108a7c62175737faa622ff599ca5
simaonogueira101 <simao.nogueira66@gmail.com>
2018-07-05 17:59:29 +0100

    Adds fils structure and starts styling.

--- parallax.js
+++
@@ -1,7 +0,0 @@
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
-parallaxInstance.friction(0.15, 0.15);
-
-var scene = document.getElementById('scene2');
-var parallaxInstance = new Parallax(scene);
-parallaxInstance.friction(0.15, 0.15);
